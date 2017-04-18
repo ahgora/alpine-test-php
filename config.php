9798829abf39dcfdbefd07d67b933baf15596f12
Weslley Camilo <weslley.azevedo@ahgora.com.br>
2017-04-18 11:22:46 -0300

    Inclusão de chaves

--- config.php
+++ config.php
@@ -3,3 +3,6 @@
 define('HOSTMONGODB', 'mongodb://mongo');
 define('REDIS_SERVER', 'redis');
 define('HOSTNSQ', 'nsqd');
+
+define('PATH_RSA_PRIVATE_KEY', 'file:///etc/config_pw2/auth.rsa');
+define('PATH_RSA_PUBLIC_KEY', 'file:///etc/config_pw2/auth.rsa.pub');
